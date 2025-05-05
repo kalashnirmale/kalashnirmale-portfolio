4a0d2bbd0b9af8d23b6be4011b435702ffa450a0
Kalash Nirmale <74041460+kalashnirmale@users.noreply.github.com>
2025-05-05 22:15:38 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js,jsx}",  // Tells Tailwind which files to scan for classes
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        emerald: "#10B981",
+        cyan: "#06B6D4",
+      },
+    },
   },
   plugins: [],
-};
+}
