4c183e9d9aaa92bbdde8522eef018dedc954df23
Kalash Nirmale <74041460+kalashnirmale@users.noreply.github.com>
2025-05-05 22:56:02 +0530

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';  // Tailwind CSS file for global styles
-import App from './App';  // Importing the main App component
+import App from './App.jsx';
+import './index.css'; // if applicable
 
-// This is the root element where the React app will be injected
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// Rendering the App component into the root div
 root.render(
   <React.StrictMode>
     <App />
