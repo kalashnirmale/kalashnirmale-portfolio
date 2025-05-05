bc3a1ca2098affa9628087b387d12f2d516866ce
Kalash Nirmale <74041460+kalashnirmale@users.noreply.github.com>
2025-05-05 21:35:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Portfolio from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
-    <Portfolio />
-  </React.StrictMode>
-)
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
